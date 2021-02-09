bad24d2e5dea188e9e47bbcffa0930b83d7845f1
nmurillo <nmurillo@gfourmis.com>
2021-02-09 16:25:32 -0500

    minor changes in the definition of variables

--- run.sh
+++ run.sh
@@ -12,13 +12,17 @@ PUBDIR=public_html
 TRGDIR=~/backups/sites
 BCKDIR=$TRGDIR/$DATE
 FILEEXT=tar.gz
+ROUTE=/usr/local/bin/aws
+filename=$TIME-$site.tar.gz
+source=$site/$PUBDIR
+destination=$finalpath/$filename
 
 BUCKET_NAME=my-aws-s3-bucket-name
 BUCKET_DIR=backups/sites
 BUCKET_PATH=$BUCKET_NAME/$BUCKET_DIR
 
 #Definir nombre de los sitios
-sites=( example.com example.org example.co )
+sites=(example.com example.org example.co)
 
 #umask 177
 
@@ -27,32 +31,27 @@ finalpath=$BCKDIR
 mkdir -p $finalpath
 
 #Recorrer sitios
-for site in "${sites[@]}"
-do
-        apppath=$WWWDIR/$site
+for site in "${sites[@]}"; do
+	apppath=$WWWDIR/$site
 
-        if [ -d "$apppath" ]; then
+	if [ -d "$apppath" ]; then
 
-	        cd $WWWDIR
-			
-			TIME=`date +%Y%m%d_%H%M%S`
-            filename=$TIME-$site.tar.gz
-			source=$site/$PUBDIR
-			destination=$finalpath/$filename
-			
-            echo "Dumping $source"
-			tar -Pcpzf $destination $source
-            tar Ozxf $destination > /dev/null			
-			status=$?
-			
-			# Validar salida del comando anterior
-			if test $status -eq 0
-			then
-				echo "Uploading $filename"
-				/usr/local/bin/aws s3 cp $destination s3://$BUCKET_PATH/$site/$DATE/files/
-			fi			
-		
-        fi
+		cd $WWWDIR
+
+		TIME=$(date +%Y%m%d_%H%M%S)
+
+		echo "Dumping $source"
+		tar -Pcpzf $destination $source
+		tar Ozxf $destination >/dev/null
+		status=$?
+
+		# Validar salida del comando anterior
+		if test $status -eq 0; then
+			echo "Uploading $filename"
+			$ROUTE s3 cp $destination s3://$BUCKET_PATH/$site/$DATE/files/
+		fi
+
+	fi
 done
 
 # ls -lh $BCKDIR
