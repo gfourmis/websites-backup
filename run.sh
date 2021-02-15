204e777e043a55bcea2b8c9182996e6a38d1b9e2
nmurillo <nmurillo@gfourmis.com>
2021-02-15 10:54:04 -0500

    correction changes in the definition of variables

--- run.sh
+++ run.sh
@@ -12,19 +12,13 @@ PUBDIR=public_html
 TRGDIR=~/backups/sites
 BCKDIR=$TRGDIR/$DATE
 FILEEXT=tar.gz
-ROUTE=/usr/local/bin/aws
-filename=$TIME-$site.tar.gz
-source=$site/$PUBDIR
-destination=$finalpath/$filename
+AWS_BIN=/usr/local/bin/aws
 
+#Definir el nombre del bucket
 BUCKET_NAME=my-aws-s3-bucket-name
-BUCKET_DIR=backups/sites
-BUCKET_PATH=$BUCKET_NAME/$BUCKET_DIR
 
 #Definir nombre de los sitios
 sites=(example.com example.org example.co)
-
-#umask 177
 
 # Crear directorio
 finalpath=$BCKDIR
@@ -34,21 +28,31 @@ mkdir -p $finalpath
 for site in "${sites[@]}"; do
 	apppath=$WWWDIR/$site
 
+	echo "Backup $apppath"
 	if [ -d "$apppath" ]; then
 
 		cd $WWWDIR
 
 		TIME=$(date +%Y%m%d_%H%M%S)
+		filename=$TIME-$site.tar.gz
+
+		destination=$finalpath/$filename
+		source=$site/$PUBDIR
 
 		echo "Dumping $source"
 		tar -Pcpzf $destination $source
 		tar Ozxf $destination >/dev/null
 		status=$?
 
+		BUCKET_DIR=backups/sites
+		BUCKET_PATH=$BUCKET_NAME/$BUCKET_DIR
+
+echo "Uploading $AWS_BIN s3 cp $destination s3://$BUCKET_PATH/$site/$DATE/files"
+
 		# Validar salida del comando anterior
 		if test $status -eq 0; then
 			echo "Uploading $filename"
-			$ROUTE s3 cp $destination s3://$BUCKET_PATH/$site/$DATE/files/
+			$AWS_BIN s3 cp $destination s3://$BUCKET_PATH/$site/$DATE/files/
 		fi
 
 	fi
