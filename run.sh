18812d3f982c4b50398a64805d73e4a8c9e40207
Felipe Rojas <frojas@gfourmis.com>
2021-03-09 09:26:18 -0500

    run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@
 #Update on 15-02-2021
 #Author = frojas
 #Version 1.1
+
 #START
 
 #Definir variables
 DATE=$(date +"%F")
 WWWDIR=/var/www
 PUBDIR=public_html
+
 FILEEXT=tar.gz
 SCRIPT_CONFIG_FILE=run.conf
 AWS_BIN=/usr/local/bin/aws
@@ -23,10 +25,13 @@ BCKDIR=$TRGDIR/$DATE
 
 #Definir nombre de los sitios
 sites=(example.com example.org example.co)
+HOST_NAME=site.com
 
 # Crear directorio
 finalpath=$BCKDIR
 mkdir -p $finalpath
+
+BUCKET_DIR=$HOST_NAME/$BUCKET_DIR
 
 #Recorrer sitios
 for site in "${sites[@]}"; do
@@ -48,10 +53,9 @@ for site in "${sites[@]}"; do
 		tar Ozxf $destination >/dev/null
 		status=$?
 
-		BUCKET_DIR=$sites/sites/$DATE
-		BUCKET_PATH=$BUCKET_NAME/$BUCKET_DIR
+		BUCKET_PATH=$BUCKET_NAME/$BUCKET_DIR/$site/$DATE
 
-echo "Uploading $AWS_BIN s3 cp $destination s3://$BUCKET_PATH/"
+		# echo "Uploading $AWS_BIN s3 cp $destination s3://$BUCKET_PATH/"
 
 		# Validar salida del comando anterior
 		if test $status -eq 0; then
@@ -62,10 +66,14 @@ echo "Uploading $AWS_BIN s3 cp $destination s3://$BUCKET_PATH/"
 	fi
 done
 
-# ls -lh $BCKDIR
+# Listar archivos
+ls -lh $BCKDIR
 
 echo "Deleting old files in $TRGDIR"
 find $TRGDIR/* -type d -ctime +7 -exec rm -rf {} \;
+
+# Listar directorios
+ls -lh $TRGDIR
 
 #Descomprimir
 #tar -xzvf archivo.tar.gz
